71de0f753c21d6be6507e69bd0f0a64d43985be9
Djama <8djama8@mail.ru>
2023-04-04 14:01:28 +0300

    mobile

---
+++ js/js.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+function toggleMenu(){
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
+
+}
